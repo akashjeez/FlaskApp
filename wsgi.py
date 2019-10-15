07ca2a9673e278339d4d3384d5025e4a1191537b
Akash Jeez <akashit63@gmail.com>
2019-10-15 15:42:40 +0530

    Latest Code

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,11 @@ application = Flask(__name__)
 
 @application.route('/')
 def index():
-	return "<h2> Deployed Flask Application to OpenShift! </h2>"
+	return "<p> Sucessfully Deployed Python Flask Web Application to OpenShift Cloud (Container Application Platform by Red Hat!) <br/><br/> <a href='/about'> About Page </a> </p>"
+
+@application.route('/about')
+def about():
+	return "<p> Hi there, I am Akash, You can Call Me AkashJeez, I Love Coding and Racing :) <br/><br/> <a href='/'> Index Page </a> </p>"
 
 if __name__ == '__main__':
-	app.run(debug = True)
+	application.run(debug = True)
